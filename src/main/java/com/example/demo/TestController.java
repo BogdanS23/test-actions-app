e1c3f6f8e70d542e59068355fb04cd594ed1e821
BogdanS23 <superbogdan23@yandex.ru>
2025-04-05 10:41:43 +0400

    Update

---
+++ src/main/java/com/example/demo/TestController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping()
+public class TestController {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping("/hello")
+    public ResponseEntity<String> hello() {
+        return ResponseEntity.ok("Hello World" + port);
+    }
+}
