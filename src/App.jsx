73e3035631561c8b3c56878c45e2ccc68f511211
BogdanS23 <superbogdan23@yandex.ru>
2025-04-05 09:09:04 +0400

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,4 +31,4 @@ function App() {
   )
 }
 
-export default Appp
+export default App
